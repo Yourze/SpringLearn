08e036bef1600408c6b994e8ffca2b689fe1b465
Zoey <787183001@qq.com>
2021-09-27 13:01:34 +0800

    spring learn demo 1

---
+++ spring5_demo1/src/com/atguigu/spring/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.atguigu.spring.dao;
+
+/**
+ * @author WangZY
+ * @date 2021年09月15日20:47
+ */
+public interface UserDao {
+    public void update();
+}
---
+++ spring5_demo1/src/com/atguigu/spring/dao/UserDaoImpl.java
@@ -0,0 +1,12 @@
+package com.atguigu.spring.dao;
+
+/**
+ * @author WangZY
+ * @date 2021年09月15日20:48
+ */
+public class UserDaoImpl implements UserDao{
+    @Override
+    public void update() {
+        System.out.println("dao update...........");
+    }
+}
